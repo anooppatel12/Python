5f0100328b61c8d73f9ebff5e823728594950f92
Anoop Patel <116958534+anooppatel12@users.noreply.github.com>
2025-01-27 07:59:11 -0800

    Leap_year_checker.py

--- conditional problem/Leap_year_checker.py
+++ conditional problem/Leap_year_checker.py
@@ -1,3 +1,5 @@
+# Determine if a year is a leap year.(leap years are divisible by 4, but not by 100 unless also divisible by 400).
+
 year = 2024
 
 if (year % 400 == 0) or (year % 4 == 0 and year % 100 != 0):
