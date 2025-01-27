433629f6328c9693efb39d572eab949ac49e88c6
Anoop Patel <116958534+anooppatel12@users.noreply.github.com>
2025-01-27 07:56:54 -0800

    Password_strength_checker.py

--- conditional problem/Password_strength_checker.py
+++ conditional problem/Password_strength_checker.py
@@ -1,3 +1,5 @@
+#Check if a password is "Weak", "Medium", or "Strong". Criteria: <6 chars(weak), 6-10 chars(medium), >10 chars(Strong).
+
 password = input("Check Your Password:")
 
 if len(password) < 6:
